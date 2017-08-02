483c8109905c64983df60482f9d3aa46fe73a1cc
khdigital <kravetzandharris@gmail.com>
2017-08-02 14:30:59 +0100

    color scheme and services updates

--- khd.js
+++ khd.js
@@ -10,28 +10,3 @@ $(document).on('click', 'a', function(event){
     }, 500);   
     }
 });
-
-function isScrolledIntoView(obj)
-{
-    var windowTop = $(window).scrollTop();
-    var windowBottom = windowTop + $(window).height();
-
-    var objTop = $(obj).offset().top;
-    var objBottom = objTop + $(obj).height();
-
-    return ((objBottom <= windowBottom) && (objTop >= windowTop));
-}
-
-
-// Check if it's time to start the animation.
-function startAnimation() {
-    var $obj = $('.animate');
-
-    // If the animation has already been started
-    if ($obj.hasClass('fade-in')) return;
-
-    if (isElementInViewport($obj)) {
-        // Start the animation
-        $obj.addClass('fade-in');
-    }
-}
